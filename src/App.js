52902bfb766ee1109fc539107dfc784dabf7ab56
Kunal Arora <10kunalji@gmail.com>
2021-02-03 17:10:57 +0530

    html part done SignupComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Router from "./Components/Router";
+// import Router from "./Components/Router";
 import { BrowserRouter } from 'react-router-dom';
+import Signup from './Components/SignupComponent'
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Router/>
+        <Signup/>
       </BrowserRouter>
     );
   }
--- src/Components/SignupComponent.js
+++ src/Components/SignupComponent.js
@@ -2,7 +2,93 @@ import React, { Component } from "react";
 
 class SignupComponent extends Component {
   render() {
-    return <h1>This is Signup Component</h1>;
+    return <div>
+              <section>
+                <div className="container box ">
+                <div className="row w-100">
+                  <div className="col-5">
+                    <div class="row pt-5">
+                      <div class="col-6">
+                          <h1 class="company pl-5"> <span class="com">Com</span>Pany</h1>
+                      </div>
+
+                          <div class="col-6 d-flex pl-5 pt-2">
+                            <h5 class="head-sign"> LOGIN </h5>
+                            <h5 class="sign-up-page">SIGNUP</h5>
+                          </div>
+                      </div>
+                  <div class="pl-5"> 
+                    <h1 class="login">SIGN UP</h1>
+
+                      <p>Sign up to continue to our application </p>
+                      <form>
+                      <div class="form-group">
+
+                            <input type="text" class="inp no-outline pt-4" placeholder="Name   " />
+                      </div>
+                      <div class="form-group">
+
+                         <input type="password" placeholder="Email" class=" inp no-outline pt-4" id="exampleInputPassword1" />
+                       </div>
+                       <div class="form-group">
+
+                          <input type="password" placeholder=" .........." class=" inp no-outline pt-4"
+                           id="exampleInputPassword1" />
+                        </div>
+                        <div class="form-group form-check pt-4">
+                          <input type="checkbox" class="form-check-input " id="exampleCheck1" />
+                           <label class="form-check-label" for="exampleCheck1">
+                          <p class="check"> By clicking sign up, You agree to our Terms of use and knowledge that you have read
+                          our Privacy Policy</p>
+                           </label>
+                        </div>
+                        <button type="submit" class="btn btn-col"><span class="log-txt-for">Signup</span></button>
+
+
+
+
+
+                      </form>
+                  
+                  
+                  
+                  
+                  </div>
+
+
+
+
+
+
+                   </div>
+
+                   <div class=" col-7">
+
+                    <img src="./assets/images/signup.svg" alt="" class="login-img" />
+
+
+                        </div>
+
+
+
+                </div>
+
+
+
+                </div>
+
+
+
+
+
+
+              </section>
+
+
+
+
+
+    </div>
   }
 }
 
