4ba3930d91e45118701d2263f4e14c71d3627e6b
Kunal <10kunalji@gmail.com>
2021-02-05 16:02:22 +0530

    full responsive signup page

--- src/Components/SignupComponent.js
+++ src/Components/SignupComponent.js
@@ -5,80 +5,86 @@ class SignupComponent extends Component {
     return (
       <React.Fragment>
         <section>
-          <div className="container box ">
-            <div className="row w-100">
-              <div className="col-5">
-                <div className="row pt-5">
-                  <div className="col-6">
-                    <h1 className="company pl-5">Health&Fitness</h1>
-                  </div>
-
-                  <div className="col-6 d-flex pl-5 pt-2">
-                    <h5 className="head-sign"> LOGIN </h5>
-                    <h5 className="sign-up-page">SIGNUP</h5>
-                  </div>
-                </div>
-                <div className="pl-5">
-                  <h1 className="login">SIGN UP</h1>
-
-                  <p>Sign up to continue to our application </p>
-                  <form>
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        className="inp no-outline pt-4"
-                        placeholder="Name   "
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        type="password"
-                        placeholder="Email"
-                        className=" inp no-outline pt-4"
-                        id="exampleInputPassword1"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        type="password"
-                        placeholder=" .........."
-                        className=" inp no-outline pt-4"
-                        id="exampleInputPassword2"
-                      />
-                    </div>
-                    <div className="form-group form-check pt-4">
-                      <input
-                        type="checkbox"
-                        className="form-check-input "
-                        id="exampleCheck1"
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="exampleCheck1"
-                      >
-                        <p className="check">
-                          {" "}
-                          By clicking sign up, You agree to our Terms of use and
-                          knowledge that you have read our Privacy Policy
-                        </p>
-                      </label>
-                    </div>
-                    <button type="submit" className="btn btn-col">
-                      <span className="log-txt-for">Signup</span>
-                    </button>
-                  </form>
-                </div>
-              </div>
-
-              <div className=" col-7">
-                <img
-                  src={process.env.PUBLIC_URL + "images/signup.svg"}
-                  alt="signup"
-                  className="login-img"
-                />
-              </div>
+        <div className="container mt-5">
+        <div className="row">
+        <div className="col-md-6 col-12 left-side">
+          <div className="row pt-5">
+            <div className="col-6 ">
+              <h1 className="company pl-5"> <span className="com">Com</span>Pany</h1>
             </div>
+
+              <div className="col-6 d-flex pl-5 pt-2">
+              <h5 className="head-sign"> LOGIN </h5>
+              <h5 className="sign-up-page">SIGNUP</h5>
+              </div>
           </div>
+          <div className="pl-5"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              height: "100%",
+              alignItems: "left",
+              flexDirection: "column",
+            }}>
+            <h1 className="login">SIGN UP</h1>
+
+            <p>Sign up to continue to our application </p>
+            <form>
+            <div className="form-group">
+
+            <input type="text" className="inp no-outline pt-4" placeholder="Name   " />
+
+            </div>
+            <div className="form-group">
+
+            <input type="password" placeholder="Email" className=" inp no-outline pt-4" id="exampleInputPassword1" />
+            </div>
+            <div className="form-group">
+
+                <input type="password" placeholder=" .........." className=" inp no-outline pt-4"
+                  id="exampleInputPassword1" />
+              </div>
+              <div className="form-group form-check pt-4">
+                <input type="checkbox" className="form-check-input " id="exampleCheck1" />
+                <label className="form-check-label" for="exampleCheck1">
+                  <p className="check"> By clicking sign up, You agree to our Terms of use and knowledge that you have read
+                    our Privacy Policy</p>
+                </label>
+              </div>
+              <button type="submit" className="btn btn-col"><span className="log-txt-for">Signup</span></button>
+            
+            </form>
+
+
+          </div>
+        
+        
+        
+        </div>
+
+
+
+
+
+        <div className="col-md-6 col-12 right-side"><img src={process.env.PUBLIC_URL + "images/signup.svg"}
+        alt="login"class="login-img" /></div>
+        
+        
+        
+        
+        
+        
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        </div>
+  
+  
         </section>
       </React.Fragment>
     );
