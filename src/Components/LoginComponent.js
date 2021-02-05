e966accf661fde13010d1eedb142a8831ef6a30f
Sanchit Bajaj <sanchit02@outlook.com>
2021-02-05 22:20:47 +0530

    bug fix

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -7,11 +7,11 @@ class LoginComponent extends Component {
         <section>
           <div className="container box ">
             <div className="row w-100">
-              <div className="col-5 col-sm-6">
+              <div className="col-5">
                 <div className="row pt-5">
                   <div className="col-6">
-                    <h1 className="company pl-5 col-md-3 ">
-                      <strong>Com</strong> Pany
+                    <h1 className="company pl-5">
+                      <span className="com">Health&Fitness</span>
                     </h1>
                   </div>
                   <div className="col-6 d-flex pl-5 pt-2">
@@ -69,11 +69,11 @@ class LoginComponent extends Component {
                 </div>
               </div>
 
-              <div className=" col-7 col-sm-6">
+              <div className=" col-7">
                 <img
                   src={process.env.PUBLIC_URL + "images/login.svg"}
                   alt="login"
-                  className="login-img col-md-12 col-sm-12" 
+                  className="login-img"
                 />
               </div>
             </div>
